ad7ae3c83a44b2681f3f631594ab13c3d827affd
Narendar14082000 <87380500+Narendar14082000@users.noreply.github.com>
2023-02-23 10:22:46 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+function sendMail() {
+    var params = {
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        email: document.getElementById("email").value,
+        subject: document.getElementById("subject").value,
+        message: document.getElementById("message").value,
+    };
+
+    const serviceID = "service_a5z84lq";
+    const templateID = "template_x73x0fe";
+
+    emailjs.send(serviceID, templateID, params)
+        .then(res => {
+            document.getElementById("name").value = "";
+            document.getElementById("email").value = "";
+            document.getElementById("Subject").value = "";
+            document.getElementById("message").value = "";
+            console.log(res);
+            alert("Your message sent successfully!!")
+
+        })
+        .catch(err => console.log(err));
+
+}
